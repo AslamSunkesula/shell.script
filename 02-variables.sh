5df6b70f6f978b492a81f29eed796f7e69db0c7e
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-17 19:39:21 +0530

    variables with date command

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DATE:$(date)
+
+echo "Hi , this script is excecuted at timestamp :$DATE" 
--- variables.sh
+++ variables.sh
@@ -3,6 +3,6 @@
 var1=$1 #this is first value form the command line
 var2=$2 #this second value from the command line 
 
-echo "hi $var1 good morning"
+echo "hi $var1 good morning " 
 echo "hello $var2 good evenig"
 
