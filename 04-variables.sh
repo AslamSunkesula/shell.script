377caab6c1350fb02a0167138ae75391d4e57c26
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-17 21:34:52 +0530

    Promptig to the user name

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,6 +5,15 @@ echo "Please enter username"
 
 read USERNAME #this vaariable the where the value of user entered 
 
-#read command wait until user submit and press enter 
+#read command Wait until the user submits and press enter.
+
 
 echo "The entered username is : $USERNAME"
+
+#Using the read command, when ever we enter the user's ability to see, we must hide it for that will use the "read -s."
+
+#We are hiding the password below :
+
+read -s PASSWORD
+
+echo "Please enter the password :$PASSWORD"
