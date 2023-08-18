22ca69ccf51470efcf68cce7443c6689af49b7f6
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-18 17:02:25 +0530

    installing the mysql package

--- 08-mysql.sh
+++ 08-mysql.sh
@@ -12,13 +12,13 @@ yum install mysql -y
 
 #it's our responsibility agin to check installation is success or not 
 
-#if [ $userid -ne 0 ]
-#then
-#echo "Installation of mysql is error"
-#    exit 2
-#else
-#echo "installation of mysql is success"
-#fi
+if [ $userid -ne 0 ]
+then
+echo "Installation of mysql is error"
+    exit 2
+else
+echo "installation of mysql is success"
+fi
 
 
 
