be729c54ac5326a4cee884a4410cfa6a77c145fc
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-18 23:15:26 +0530

    installing the mysql package and git package by using the functions

--- 01-functions.sh
+++ 01-functions.sh
@@ -5,7 +5,7 @@ DATE=$(date +%m-%d-%Y)
 
 SCRIPT_NAME=$0 #$0 will tell us the script name 
 
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+#LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
 VALIDATE() {   # this is a function to check the fail or pass 
 
@@ -28,9 +28,9 @@ fi
 
 yum install mysql -y
 
-VALIDATE $? "Insatalling mysql" &>>$LOGFILE
+VALIDATE $? "Insatalling mysql" ##&>>$LOGFILE
 #passing the 2 arguments to the validate function and $? --> passing input to the validate function to check previous command fail or pass
 
 yum install git -y
 
-VALIDATE $? "Insatalling  git" &>>$LOGFILE
+VALIDATE $? "Insatalling  git" ##&>>$LOGFILE
