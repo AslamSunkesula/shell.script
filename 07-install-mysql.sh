f68d1b31b02e1012285f3cce5ceac51c1c1a7da1
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-18 16:51:13 +0530

    installing the mysql package

--- 07-install-mysql.sh
+++ 07-install-mysql.sh
@@ -9,10 +9,17 @@ echo "Error: You are not a root user"
 exit 1
 fi
 
-yum install mysqll -y
+yum install mysql -y
 
 #it's our responsibility agin to check installation is success or not 
 
+#if [ $userid -ne 0 ]
+#then
+#echo "Installation of mysql is error"
+#    exit 2
+#else
+#echo "installation of mysql is success"
+#fi
 
 
 
