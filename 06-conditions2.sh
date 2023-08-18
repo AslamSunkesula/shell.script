8fcd6331275cdd9f09ebcabdf60d8ca87c7e5614
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-18 13:03:13 +0530

    condition using equality check

---
+++ 06-conditions2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+num1=$1
+num2=$2
+
+
+
+if [ $num1 -eq $num2 ] #-eq equality check
+then
+
+echo  "Both the variables are same" ] 
+
+else 
+
+    echo "Both the variables are not same"
+
+fi      
