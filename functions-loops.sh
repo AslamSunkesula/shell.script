209cf6944aa1b194cfabb9f11615de7b7cc77f40
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-21 20:16:59 +0530

    Implementing the validations

--- functions-loops.sh
+++ functions-loops.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-
 DATE=$(date +%F)
-LOGSDIR=/home/centos/shellscript-logs
-# /home/centos/shellscript-logs/script-name-date.log
-SCRIPT_NAME=$0
+LOGSDIR=/c/Users/assua/shellscript-logs
 
-LOGFILE=$LOGSDIR/$0-$DATE.log
+script_name=$0
+LOGFILE=$LOGSDIR/$script_name-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-if [ $USERID -ne 0 ];
-then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1
-fi
 
 VALIDATE(){
     if [ $1 -ne 0 ];
@@ -28,18 +21,18 @@ VALIDATE(){
     fi
 }
 
-# all args are in $@
-for i in $@
-do
-    yum list installed $i &>>$LOGFILE
-    if [ $? -ne 0 ]
-    then
-        echo "$i is not installed, let's install it"
-        yum install $i -y &>>$LOGFILE
-        VALIDATE $? "$i"
-    else
-        echo -e "$Y $i is already installed $N"
-    fi
 
-    #yum install $i -y
-done
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+yum install git -y &>>$LOGFILE
+
+VALIDATE $? "GIT"
+
+
+
+
+# all args are in $@
