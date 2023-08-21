6134822126abacd8168fcc9c9f6d1746d226fe9e
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-21 15:57:53 +0530

    loops and functions

--- function-prac.sh
+++ function-prac.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+
+DATE=$(date +%F)
+
 USERID=$(id -u)
 
 r="\e[31m"
 
 g="\e[32m"
+
+LOGSDIR=/home/centos/shellscript-logs
+
+script_name=$0
+
+LOGFILE=$LOGSDIR/$script_name/$DATE.log
 
 
 VALIDATE() {
@@ -26,11 +35,11 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-yum install git -y 
+yum install git -y &>>$LOGFILE
 
 VALIDATE $? " Installig the git"
 
-yum install nginx -y 
+yum install nginx -y &>>LOGFILE
 
 VALIDATE $? " Installig the nginx"
 
