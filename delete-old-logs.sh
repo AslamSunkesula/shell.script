d84c896d3dd0d367b39845ca899552c378adf88d
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-23 18:30:17 +0530

    deleting the old files

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -4,7 +4,7 @@ APP_logs=/home/centos/app-logs
 DATE=$(date +%F)
 LOGSDIR=/home/centos/logs
 script_name=$0
-LOGFILE=$LOGSDIR/$script_name-$DATE.log
+LOGFILE=$LOGSDIR/$script_name-$DATE.log 
 
 
 find_to_delete=$(find $APP_logs -name "*.log" -type f -mtime +14)
