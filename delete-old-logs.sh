348563c2a9e179a9f93b8b553ab02bdcb863ef75
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-23 21:33:40 +0530

    find to delete old files

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -7,7 +7,7 @@ script_name=$0
 LOGFILE=$LOGSDIR/$script_name-$DATE.log 
 
 
-find_to_delete=$(find APP_logs -name "*.log" -type f -mtime +14)
+find_to_delete=$(find $APP_logs -name "*.log" -type f -mtime +14)
 
 
     echo "$find_to_delete"
