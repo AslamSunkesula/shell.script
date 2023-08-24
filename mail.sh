0b853eb075fd06fddcda9b46ebfb6e396e42ce4f
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-24 20:01:54 +0530

    find the the disk usage

--- mail.sh
+++ mail.sh
@@ -11,7 +11,6 @@ TEAM_NAME=$4
 ALERT_TYPE=$5
 
 #echo "all args: $@"
-
 FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
 
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
