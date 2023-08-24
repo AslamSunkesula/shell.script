0c9acb04f49ddbfafbc363d9ac5f645d9441352d
Aslam sunkesula <aslam.sukesula@gmail.com>
2023-08-24 17:07:57 +0530

    find the the disk usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -8,15 +8,21 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 
+DISK_USAGE_THRESHOLD=1
 
-# DISK_USAGE_THRESHOLD=1
+while IFS= read line; do
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1}')
+    #now you need to check whether it is more than threshold or not
+    if [ $usage -gt $DISK_USAGE_THRESHOLD ]; then
 
+        message+="HIGH DISK USAGE on $partition: $usage\n"
+    fi
 
-while IFS= read line
-do 
-echo "output : $line"
+    echo
 
-done <<< $DISK_USAGE
+done <<<$DISK_USAGE
+
+echo "message : $message"
